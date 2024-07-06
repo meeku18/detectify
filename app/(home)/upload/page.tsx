85f32afaa3fa8fb6406638b9b72f6d9c5c9139ab
meeku18 <97672118+meeku18@users.noreply.github.com>
2024-07-06 18:01:36 +0530

    Update page.tsx

--- app/(home)/upload/page.tsx
+++ app/(home)/upload/page.tsx
@@ -72,7 +72,7 @@ export default function Upload() {
             <AccordionItem value="item-3">
               <AccordionTrigger>Is it animated?</AccordionTrigger>
               <AccordionContent>
-                Yes. It's animated by default, but you can disable it if you prefer.
+                Yes. It&apos; animated by default, but you can disable it if you prefer.
               </AccordionContent>
             </AccordionItem>
           </Accordion>
